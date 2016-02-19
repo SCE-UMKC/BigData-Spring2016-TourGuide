e4d3c11d88f439df2382960cf73d3a4bc247d016
smoeller1 <simon@quasily.com>
2016-02-19 07:11:27 -0600

    increment 1 work

---
+++ src/ImageId/src/ImageId/Image.java
@@ -0,0 +1,66 @@
+package ImageId;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.features2d.DescriptorExtractor;
+import org.opencv.features2d.FeatureDetector;
+import org.opencv.imgcodecs.Imgcodecs;
+
+public class Image {
+	private String imageSrc;
+	private Mat imageMat;
+	private MatOfKeyPoint imageKP;
+	private String imageDesc;
+	private Mat imageDescriptor;
+	private static int featureDetector = FeatureDetector.ORB;
+	private static int descriptorExtractor = DescriptorExtractor.ORB;
+	
+	public Image(String imageSrc) {
+		this(imageSrc, "Unknown");
+	}
+	public Image(String imageSrc, String imageDesc) {
+		super();
+		this.imageSrc = imageSrc;
+		this.imageDesc = imageDesc;
+		this.imageKP = new MatOfKeyPoint();
+		this.imageDescriptor = new Mat();
+		this.imageMat = ReadImage();
+		DetectKP();
+		ComputeDescriptor();
+	}
+	
+	public String getImageSrc() {
+		return imageSrc;
+	}
+	public void setImageSrc(String imageSrc) {
+		this.imageSrc = imageSrc;
+	}
+	public MatOfKeyPoint getImageKP() {
+		return imageKP;
+	}
+	public String getImageDesc() {
+		return imageDesc;
+	}
+	public Mat getImageDescriptor() {
+		return imageDescriptor;
+	}
+	
+	public int getImageCols() {
+		return imageMat.cols();
+	}
+	public int getImageRows() {
+		return imageMat.rows();
+	}
+
+	private Mat ReadImage() {
+		return Imgcodecs.imread(imageSrc);
+	}
+	private void DetectKP() {
+		FeatureDetector detector = FeatureDetector.create(featureDetector);
+		detector.detect(imageMat, imageKP);
+	}
+	private void ComputeDescriptor() {
+		DescriptorExtractor de = DescriptorExtractor.create(descriptorExtractor);
+		de.compute(imageMat, imageKP, imageDescriptor);
+	}
+}
