13762f30e1e477ae75b2a7a8564ddb9c6db746a0
smoeller1 <simon@quasily.com>
2016-04-29 09:54:10 -0500

    A little cleanup

--- src/main/java/TourGuide.java
+++ src/main/java/TourGuide.java
@@ -2,6 +2,8 @@
 import org.json.JSONObject;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +15,6 @@ public class TourGuide {
     public static void main(String[] args) {
         System.out.println("main: Starting");
 
-        /*
         List<String> imgList = new ArrayList<String>();
         imgList.add("IMG_5480.JPG");
         imgList.add("IMG_5481.JPG");
@@ -27,12 +28,17 @@ public class TourGuide {
             e.printStackTrace();
         }
         System.out.println("main: Finished stitching, output image: " + panoImage);
-        */
+
 
         YelpRecommend recommender = new YelpRecommend();
 
         String searchItem = "belt";
         JSONObject results = recommender.searchForBusinessesByLocation(searchItem, "39.04229,-94.59155");
         System.out.println("Best match business for " + searchItem + ": " + results.get("name") + ", " + results.get("distance") + " meters away at (" + results.get("latitude") + "," + results.get("longitude") + ")");
+        try {
+            SocketClient.sendToServer("Best match business for " + searchItem + ": " + results.get("name") + ", " + results.get("distance") + " meters away at (" + results.get("latitude") + "," + results.get("longitude") + ")");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
