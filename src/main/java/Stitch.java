5a6bdfe177133648165fa38dc8a3d51c3e84e316
smoeller1 <simon@quasily.com>
2016-04-29 16:15:29 -0500

    More increment 4 checkins

--- src/main/java/Stitch.java
+++ src/main/java/Stitch.java
@@ -5,6 +5,7 @@ import boofcv.abst.feature.detdesc.DetectDescribePoint;
 import boofcv.abst.feature.detect.interest.ConfigFastHessian;
 import boofcv.alg.descriptor.UtilFeature;
 import boofcv.alg.distort.ImageDistort;
+import boofcv.alg.distort.ImageDistortBasic;
 import boofcv.alg.distort.PixelTransformHomography_F32;
 import boofcv.alg.distort.impl.DistortSupport;
 import boofcv.alg.interpolate.InterpolatePixelS;
@@ -18,16 +19,17 @@ import boofcv.gui.image.ShowImages;
 import boofcv.io.UtilIO;
 import boofcv.io.image.ConvertBufferedImage;
 import boofcv.io.image.UtilImageIO;
+import boofcv.struct.distort.PixelTransform_F32;
 import boofcv.struct.feature.AssociatedIndex;
 import boofcv.struct.feature.BrightFeature;
 import boofcv.struct.feature.TupleDesc;
 import boofcv.struct.geo.AssociatedPair;
-//import boofcv.struct.image.ImageFloat32;
-import boofcv.struct.image.GrayF32;
-//import boofcv.struct.image.ImageSingleBand;
-//import boofcv.struct.image.MultiSpectral;
-import boofcv.struct.image.ImageGray;
-import boofcv.struct.image.Planar;
+//import boofcv.struct.image.GrayF32;
+import boofcv.struct.image.ImageFloat32;
+//import boofcv.struct.image.ImageGray;
+import boofcv.struct.image.ImageSingleBand;
+import boofcv.struct.image.MultiSpectral;
+//import boofcv.struct.image.Planar;
 import georegression.struct.homography.Homography2D_F64;
 import georegression.struct.point.Point2D_F64;
 import georegression.struct.point.Point2D_I32;
@@ -122,51 +124,41 @@ public class Stitch {
 
         BufferedImage imageA = imageS.get(0);
         System.out.println("stringit: " + imageS.toString());
-        //ImageSingleBand inputA = ConvertBufferedImage.convertFromSingle(imageA, null, ImageFloat32.class);
-        GrayF32 inputA = ConvertBufferedImage.convertFromSingle(imageA, null, GrayF32.class);
+        ImageFloat32 inputA = ConvertBufferedImage.convertFromSingle(imageA, null, ImageFloat32.class);
+        //GrayF32 inputA = ConvertBufferedImage.convertFromSingle(imageA, null, GrayF32.class);
 
         for(int i = 1; i < imageS.size(); i++) {
 
-            //DetectDescribePoint detDesc = FactoryDetectDescribe.surfStable(new ConfigFastHessian(1, 2, 200, 1, 9, 4, 4), null,null, ImageFloat32.class);
-            DetectDescribePoint detDesc = FactoryDetectDescribe.surfStable(new ConfigFastHessian(1, 2, 200, 1, 9, 4, 4), null,null, GrayF32.class);
+            BufferedImage imageB = (BufferedImage) imageS.get(i);
+            ImageFloat32 inputB = ConvertBufferedImage.convertFromSingle(imageB, null, ImageFloat32.class);
+
+            DetectDescribePoint detDesc = FactoryDetectDescribe.surfStable(new ConfigFastHessian(1, 2, 200, 1, 9, 4, 4), null, null, ImageFloat32.class);
+            //DetectDescribePoint detDesc = FactoryDetectDescribe.surfStable(new ConfigFastHessian(1, 2, 200, 1, 9, 4, 4), null,null, GrayF32.class);
             ScoreAssociation<BrightFeature> scorer = FactoryAssociation.scoreEuclidean(BrightFeature.class, true);
             AssociateDescription<BrightFeature> associate = FactoryAssociation.greedy(scorer,2,true);
 
             // fit the images using a homography.  This works well for rotations and distant objects.
-            ModelMatcher<Homography2D_F64,AssociatedPair> modelMatcher = FactoryMultiViewRobust.homographyRansac(null,new ConfigRansac(60,3));
+            ModelMatcher<Homography2D_F64,AssociatedPair> modelMatcher = FactoryMultiViewRobust.homographyRansac(null, new ConfigRansac(60,3));
 
-            BufferedImage imageB = (BufferedImage) imageS.get(i);
-            //ImageSingleBand inputB = ConvertBufferedImage.convertFromSingle(imageB, null, ImageFloat32.class);
-            GrayF32 inputB = ConvertBufferedImage.convertFromSingle(imageB, null, GrayF32.class);
-            System.out.println("inputB: " + inputB.toString());
-            //Homography2D_F64 H = computeTransform(inputA, inputB, detDesc, associate, modelMatcher);
+            //GrayF32 inputB = ConvertBufferedImage.convertFromSingle(imageB, null, GrayF32.class);
             Homography2D_F64 H = computeTransform(inputA, inputB, detDesc, associate, modelMatcher);
             String tmpImgName = generateRandomImageName();
 
-            stitch(imageA, imageB, GrayF32.class);
-            renderStitching(imageA, imageB, H);
+            //stitch(imageA, imageB, GrayF32.class);
+            //renderStitching(imageA, imageB, H);
 
             WriteImage(imageA, imageB, H, tmpImgName);
             middleImages.add(tmpImgName);
             System.out.println("Created new merged image " + tmpImgName);
-
-            try {
-                Thread.sleep(10000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            System.exit(1);
         }
         return middleImages;
     }
 
-    /*
+
     private static<FD extends TupleDesc> Homography2D_F64 computeTransform(
-            GrayF32 imageA, GrayF32 imageB, DetectDescribePoint<GrayF32,FD> detDesc,
-            //ImageSingleBand imageA, ImageSingleBand imageB, DetectDescribePoint<ImageSingleBand,FD> detDesc,
-            AssociateDescription<FD> associate,
-            ModelMatcher<Homography2D_F64,AssociatedPair> modelMatcher) {
+            //GrayF32 imageA, GrayF32 imageB, DetectDescribePoint<GrayF32,FD> detDesc,
+            ImageFloat32 imageA, ImageFloat32 imageB, DetectDescribePoint<ImageFloat32,FD> detDesc,
+            AssociateDescription<FD> associate, ModelMatcher<Homography2D_F64,AssociatedPair> modelMatcher) {
         // get the length of the description
         List<Point2D_F64> pointsA = new ArrayList<Point2D_F64>();
         FastQueue<FD> descA = UtilFeature.createQueue(detDesc, 100);
@@ -202,11 +194,11 @@ public class Stitch {
         // return the found image transform
         return modelMatcher.getModelParameters().copy();
     }
-    */
+
 
     private static<FD extends TupleDesc> void describeImage(
-            //ImageSingleBand image, DetectDescribePoint<ImageSingleBand,FD> detDesc,
-            GrayF32 image, DetectDescribePoint<GrayF32,FD> detDesc,
+            ImageFloat32 image, DetectDescribePoint<ImageFloat32,FD> detDesc,
+            //GrayF32 image, DetectDescribePoint<GrayF32,FD> detDesc,
             List<Point2D_F64> points, FastQueue<FD> listDescs) {
         detDesc.detect(image);
 
@@ -228,25 +220,18 @@ public class Stitch {
     }
 
     private static void WriteImage(BufferedImage imageA, BufferedImage imageB, Homography2D_F64 fromAtoB, String outputImg) {
-        UtilImageIO.saveImage(imageA, (String) "c:\\img\\tmp1.png");
-        UtilImageIO.saveImage(imageB, (String) "c:\\img\\tmp2.png");
-
         // specify size of output image
         double scale = 0.5;
 
         // Convert into a BoofCV color format
-        //MultiSpectral<ImageFloat32> colorA = ConvertBufferedImage.convertFromMulti(imageA, null, true, ImageFloat32.class);
-        //MultiSpectral<ImageFloat32> colorB = ConvertBufferedImage.convertFromMulti(imageB, null, true, ImageFloat32.class);
-        Planar<GrayF32> colorA = ConvertBufferedImage.convertFromMulti(imageA, null, true, GrayF32.class);
-        Planar<GrayF32> colorB = ConvertBufferedImage.convertFromMulti(imageB, null, true, GrayF32.class);
-
-        /*Planar<GrayF32> colorA = ConvertBufferedImage.convertFromMulti(imageA, null, true, GrayF32.class);
-        Planar<GrayF32> colorB = ConvertBufferedImage.convertFromMulti(imageB, null,true, GrayF32.class);
-        */
+        MultiSpectral<ImageFloat32> colorA = ConvertBufferedImage.convertFromMulti(imageA, null, true, ImageFloat32.class);
+        MultiSpectral<ImageFloat32> colorB = ConvertBufferedImage.convertFromMulti(imageB, null, true, ImageFloat32.class);
+        //Planar<GrayF32> colorA = ConvertBufferedImage.convertFromMulti(imageA, null, true, GrayF32.class);
+        //Planar<GrayF32> colorB = ConvertBufferedImage.convertFromMulti(imageB, null, true, GrayF32.class);
 
         // Where the output images are rendered into
-        //MultiSpectral<GrayF32> work = colorA.createSameShape();
-        Planar<GrayF32> work = colorA.createSameShape();
+        MultiSpectral<ImageFloat32> work = colorA.createSameShape();
+        //Planar<GrayF32> work = colorA.createSameShape();
 
         // Adjust the transform so that the whole image can appear inside of it
         Homography2D_F64 fromAToWork = new Homography2D_F64(scale, 0, colorA.width/4, 0, scale, colorA.height/4, 0, 0, 1);
@@ -254,10 +239,10 @@ public class Stitch {
 
         // Used to render the results onto an image
         PixelTransformHomography_F32 model = new PixelTransformHomography_F32();
-        //InterpolatePixelS<ImageFloat32> interp = FactoryInterpolation.bilinearPixelS(ImageFloat32.class, BorderType.VALUE);
-        InterpolatePixelS<GrayF32> interp = FactoryInterpolation.bilinearPixelS(GrayF32.class, BorderType.ZERO);
-        //ImageDistort<MultiSpectral<ImageFloat32>,MultiSpectral<ImageFloat32>> distort = DistortSupport.createDistortMS(ImageFloat32.class, model, interp, false);
-        ImageDistort<Planar<GrayF32>,Planar<GrayF32>> distort = DistortSupport.createDistortPL(GrayF32.class, model, interp, false);
+        InterpolatePixelS<ImageFloat32> interp = FactoryInterpolation.bilinearPixelS(ImageFloat32.class, BorderType.VALUE);
+        //InterpolatePixelS<GrayF32> interp = FactoryInterpolation.bilinearPixelS(GrayF32.class, BorderType.ZERO);
+        ImageDistort<MultiSpectral<ImageFloat32>,MultiSpectral<ImageFloat32>> distort = DistortSupport.createDistortMS(ImageFloat32.class, model, interp, false);
+        //ImageDistort<Planar<GrayF32>,Planar<GrayF32>> distort = DistortSupport.createDistortPL(GrayF32.class, model, interp, false);
         distort.setRenderAll(false);
 
         // Render first image
@@ -270,153 +255,11 @@ public class Stitch {
         distort.apply(colorB,work);
 
         // Convert the rendered image into a BufferedImage
-        BufferedImage output = new BufferedImage(work.width,work.height,imageA.getType());
-
-        UtilImageIO.saveImage(output, "c:\\img\\tmp3.png");
+        BufferedImage output = new BufferedImage(work.width, work.height, imageA.getType());
 
         ConvertBufferedImage.convertTo(work,output,true);
 
         UtilImageIO.saveImage(output, outputImg);
         //ShowImages.showWindow(output,"Stitched Images", true);
-    }
-
-
-
-    public static void renderStitching(BufferedImage imageA, BufferedImage imageB, Homography2D_F64 fromAtoB) {
-        // specify size of output image
-        double scale = 0.5;
-
-        // Convert into a BoofCV color format
-        Planar<GrayF32> colorA =
-                ConvertBufferedImage.convertFromMulti(imageA, null, true, GrayF32.class);
-        Planar<GrayF32> colorB =
-                ConvertBufferedImage.convertFromMulti(imageB, null,true, GrayF32.class);
-
-        // Where the output images are rendered into
-        Planar<GrayF32> work = colorA.createSameShape();
-
-        // Adjust the transform so that the whole image can appear inside of it
-        Homography2D_F64 fromAToWork = new Homography2D_F64(scale,0,colorA.width/4,0,scale,colorA.height/4,0,0,1);
-        Homography2D_F64 fromWorkToA = fromAToWork.invert(null);
-
-        // Used to render the results onto an image
-        PixelTransformHomography_F32 model = new PixelTransformHomography_F32();
-        InterpolatePixelS<GrayF32> interp = FactoryInterpolation.bilinearPixelS(GrayF32.class, BorderType.ZERO);
-        ImageDistort<Planar<GrayF32>,Planar<GrayF32>> distort =
-                DistortSupport.createDistortPL(GrayF32.class, model, interp, false);
-        distort.setRenderAll(false);
-
-        // Render first image
-        model.set(fromWorkToA);
-        distort.apply(colorA,work);
-
-        // Render second image
-        Homography2D_F64 fromWorkToB = fromWorkToA.concat(fromAtoB,null);
-        model.set(fromWorkToB);
-        distort.apply(colorB,work);
-
-        // Convert the rendered image into a BufferedImage
-        BufferedImage output = new BufferedImage(work.width,work.height,imageA.getType());
-        ConvertBufferedImage.convertTo(work,output,true);
-
-        Graphics2D g2 = output.createGraphics();
-
-        // draw lines around the distorted image to make it easier to see
-        Homography2D_F64 fromBtoWork = fromWorkToB.invert(null);
-        Point2D_I32 corners[] = new Point2D_I32[4];
-        corners[0] = renderPoint(0,0,fromBtoWork);
-        corners[1] = renderPoint(colorB.width,0,fromBtoWork);
-        corners[2] = renderPoint(colorB.width,colorB.height,fromBtoWork);
-        corners[3] = renderPoint(0,colorB.height,fromBtoWork);
-
-        g2.setColor(Color.ORANGE);
-        g2.setStroke(new BasicStroke(4));
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g2.drawLine(corners[0].x,corners[0].y,corners[1].x,corners[1].y);
-        g2.drawLine(corners[1].x,corners[1].y,corners[2].x,corners[2].y);
-        g2.drawLine(corners[2].x,corners[2].y,corners[3].x,corners[3].y);
-        g2.drawLine(corners[3].x,corners[3].y,corners[0].x,corners[0].y);
-
-        ShowImages.showWindow(output,"Stitched Images", true);
-    }
-
-    private static Point2D_I32 renderPoint( int x0 , int y0 , Homography2D_F64 fromBtoWork )
-    {
-        Point2D_F64 result = new Point2D_F64();
-        HomographyPointOps_F64.transform(fromBtoWork, new Point2D_F64(x0, y0), result);
-        return new Point2D_I32((int)result.x,(int)result.y);
-    }
-
-    public static <T extends ImageGray>
-    void stitch( BufferedImage imageA , BufferedImage imageB , Class<T> imageType )
-    {
-        T inputA = ConvertBufferedImage.convertFromSingle(imageA, null, imageType);
-        T inputB = ConvertBufferedImage.convertFromSingle(imageB, null, imageType);
-
-        // Detect using the standard SURF feature descriptor and describer
-        DetectDescribePoint detDesc = FactoryDetectDescribe.surfStable(
-                new ConfigFastHessian(1, 2, 200, 1, 9, 4, 4), null,null, imageType);
-        ScoreAssociation<BrightFeature> scorer = FactoryAssociation.scoreEuclidean(BrightFeature.class,true);
-        AssociateDescription<BrightFeature> associate = FactoryAssociation.greedy(scorer,2,true);
-
-        // fit the images using a homography.  This works well for rotations and distant objects.
-        ModelMatcher<Homography2D_F64,AssociatedPair> modelMatcher =
-                FactoryMultiViewRobust.homographyRansac(null,new ConfigRansac(60,3));
-
-        Homography2D_F64 H = computeTransform(inputA, inputB, detDesc, associate, modelMatcher);
-
-        renderStitching(imageA,imageB,H);
-    }
-
-    public static<T extends ImageGray, FD extends TupleDesc> Homography2D_F64
-    computeTransform(T imageA, T imageB, DetectDescribePoint<T,FD> detDesc, AssociateDescription<FD> associate, ModelMatcher<Homography2D_F64,AssociatedPair> modelMatcher) {
-        // get the length of the description
-        List<Point2D_F64> pointsA = new ArrayList<Point2D_F64>();
-        FastQueue<FD> descA = UtilFeature.createQueue(detDesc,100);
-        List<Point2D_F64> pointsB = new ArrayList<Point2D_F64>();
-        FastQueue<FD> descB = UtilFeature.createQueue(detDesc,100);
-
-        // extract feature locations and descriptions from each image
-        describeImage(imageA, detDesc, pointsA, descA);
-        describeImage(imageB, detDesc, pointsB, descB);
-
-        // Associate features between the two images
-        associate.setSource(descA);
-        associate.setDestination(descB);
-        associate.associate();
-
-        // create a list of AssociatedPairs that tell the model matcher how a feature moved
-        FastQueue<AssociatedIndex> matches = associate.getMatches();
-        List<AssociatedPair> pairs = new ArrayList<AssociatedPair>();
-
-        for( int i = 0; i < matches.size(); i++ ) {
-            AssociatedIndex match = matches.get(i);
-
-            Point2D_F64 a = pointsA.get(match.src);
-            Point2D_F64 b = pointsB.get(match.dst);
-
-            pairs.add( new AssociatedPair(a,b,false));
-        }
-
-        // find the best fit model to describe the change between these images
-        if( !modelMatcher.process(pairs) )
-            throw new RuntimeException("Model Matcher failed!");
-
-        // return the found image transform
-        return modelMatcher.getModelParameters().copy();
-    }
-
-    private static <T extends ImageGray, FD extends TupleDesc>
-    void describeImage(T image,
-                       DetectDescribePoint<T,FD> detDesc,
-                       List<Point2D_F64> points,
-                       FastQueue<FD> listDescs) {
-        detDesc.detect(image);
-
-        listDescs.reset();
-        for( int i = 0; i < detDesc.getNumberOfFeatures(); i++ ) {
-            points.add( detDesc.getLocation(i).copy() );
-            listDescs.grow().setTo(detDesc.getDescription(i));
-        }
     }
 }
--- src/main/java/TourGuide.java
+++ src/main/java/TourGuide.java
@@ -1,10 +1,14 @@
 
 import org.json.JSONObject;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -15,12 +19,55 @@ public class TourGuide {
     public static void main(String[] args) {
         System.out.println("main: Starting");
 
-        List<String> imgList = new ArrayList<String>();
+        //Initial setup
+        String workingDir = "c:\\img\\";
+        int standardHeight = 500;
+        Double lat = 39.042349;
+        Double lon = -94.588234;
+
+        File folder = new File(workingDir);
+        File[] listOfFiles = folder.listFiles();
+        List<String> imgList = new ArrayList<String>(Arrays.asList(folder.list()));
+
+        //List of images to work on
+        /* List<String> imgList = new ArrayList<String>();
         imgList.add("IMG_5480.JPG");
         imgList.add("IMG_5481.JPG");
         imgList.add("IMG_5482.JPG");
-        imgList.add("IMG_5483.JPG");
-        Stitch myStitcher = new Stitch(imgList, "c:\\img\\");
+        imgList.add("IMG_5483.JPG"); */
+
+
+        //resize the images to a standard size
+        System.out.println("Total No of Files:"+listOfFiles.length);
+        BufferedImage img = null;
+        BufferedImage tempJPG = null;
+        File newFileJPG = null;
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println("Resizing File " + folder.getPath() + listOfFiles[i].getName());
+                int pos = listOfFiles[i].getName().lastIndexOf(".");
+                try {
+                    img = ImageIO.read(new File(folder.getPath() + "\\" + listOfFiles[i].getName()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                int newWidth = img.getWidth() * standardHeight / img.getHeight();
+                tempJPG = ImageResizer.resizeImage(img, newWidth, standardHeight);
+                newFileJPG = new File(folder.getPath() + "\\" + listOfFiles[i].getName().substring(0,pos)+"_New.jpg");
+                try {
+                    ImageIO.write(tempJPG, "jpg", newFileJPG);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("    Resized to " + newFileJPG);
+            }
+        }
+
+
+
+        //Stitch the images together
+        Stitch myStitcher = new Stitch(imgList, workingDir);
         String panoImage = "";
         try {
             panoImage = myStitcher.OutputImage();
@@ -32,11 +79,12 @@ public class TourGuide {
 
         YelpRecommend recommender = new YelpRecommend();
 
-        String searchItem = "belt";
-        JSONObject results = recommender.searchForBusinessesByLocation(searchItem, "39.04229,-94.59155");
-        System.out.println("Best match business for " + searchItem + ": " + results.get("name") + ", " + results.get("distance") + " meters away at (" + results.get("latitude") + "," + results.get("longitude") + ")");
+        String searchItem = "ice cream";
+        JSONObject results = recommender.searchForBusinessesByLocation(searchItem, lat.toString()+","+lon.toString());
+        String clientMessage = "Best match business for " + searchItem + ": " + results.get("name") + ", " + results.get("distance") + " meters away at (" + results.get("latitude") + "," + results.get("longitude") + ")";
+        System.out.println(clientMessage);
         try {
-            SocketClient.sendToServer("Best match business for " + searchItem + ": " + results.get("name") + ", " + results.get("distance") + " meters away at (" + results.get("latitude") + "," + results.get("longitude") + ")");
+            SocketClient.sendToServer(clientMessage + "\n", "10.151.3.35", 1234);
         } catch (IOException e) {
             e.printStackTrace();
         }
